359ef266f8594c682660ecbff9045fbbdb4cd4d9
Luis <peredaluismartin@gmail.com>
2020-09-04 19:10:14 -0700

    Python v2 to v3

--- Chapter01/formatted_output2.py
+++ Chapter01/formatted_output2.py
@@ -1,4 +1,4 @@
-print ("Name Marks Age")
-print ( "%s %14.2f %11d" % ("John Doe", 80.67, 27)) 
-print ( "%s %12.2f %11d" %("Bhaskar" ,76.901, 27))
-print ( "%s %3.2f %11d" %("Mohit", 56.98, 25)) 
+print ( "%s %14s %12s" % ("Name", "Marks", "Age"))
+print ( "%s %10.2f %11d" % ("John Doe", 80.67, 27))
+print ( "%s %11.2f %11d" %("Bhaskar" ,76.901, 27))
+print ( "%s %13.2f %11d" %("Mohit", 56.98, 25))
